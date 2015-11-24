f86754d9967b5a14f059f82b59aa4177664beecb
Ril Jian <rilislearning@gmail.com>
2015-11-24 23:06:10 +0800

    Index notify by gmail

--- project2/index_notify/signup/views.py
+++ project2/index_notify/signup/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 import requests
+import httplib2
+import os
+import argparse
+import oauth2client
+from oauth2client import tools
+from oauth2client import client
+from . import sendEmail
+from apiclient import discovery
 
 # Create your views here.
 
@@ -24,6 +32,26 @@ def send_simple_message(request):
                     send = float(index) > float(request.GET['index'])
 
     if send:
+        message = sendEmail.CreateMessage(sender='rilislearning@gmail.com',
+                to=request.GET['email'],
+                subject='Index notify',
+                message_text=(request.GET['what'] + ' is ' + request.GET['operator'] + ' ' + request.GET['index']))
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir,
+                                       'gmail-python-quickstart.json')
+        store = oauth2client.file.Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+            flow.user_agent = APPLICATION_NAME
+            credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('gmail', 'v1', http=http)
+        sendEmail.SendMessage(service=service, user_id='rilislearning@gmail.com', message=message)
         return HttpResponse('Success!');
     else:
         return HttpResponse('Oops!');
