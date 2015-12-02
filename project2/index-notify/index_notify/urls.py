b489947ef875f16f6fe24861ba1a0baeabef758c
Ril Jian <rilislearning@gmail.com>
2015-12-02 23:23:31 +0800

    Migrate to Heroku

--- project2/index-notify/index_notify/urls.py
+++ project2/index-notify/index_notify/urls.py
@@ -1,7 +1,7 @@
 """index_notify URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -11,9 +11,10 @@ Class-based views
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 from register.views import load_page
 from register.views import register
@@ -21,7 +22,7 @@ from register.views import load_registerd_data
 from register.views import load_financial_data
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^load_page$', load_page),
     url(r'^register$', register),
     url(r'^load_registered_data$', load_registerd_data),
