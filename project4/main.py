3bd7d7b2fdc3b92d2654db5934117635aebb6cfd
Ril Jian <rilislearning@gmail.com>
2015-12-31 02:53:08 +0800

    Find out the best number of considered variables

--- project4/main.py
+++ project4/main.py
@@ -1,36 +1,72 @@
 import csv
+import numpy
+import copy
 from sklearn.neural_network import MLPClassifier
 
-CATA = (
-    'BMI',
-    'Wt',
-    'Medical_History_23',
-    'Medical_Keyword_15',
-    'Medical_Keyword_3',
-    'Medical_History_4',
-    'Medical_History_39',
-    'Ins_Age',
-    'Product_Info_4'
+SPEC_CATA = (
+    'Id',
+    'Product_Info_2',
+    'Response'
 )
+NUM_CONSIDERED_CATA = 36
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+considered_cata = []
 with open('input/train.csv', newline='') as train_file:
     reader = csv.DictReader(train_file)
-    clf = MLPClassifier(verbose=True)
+    train_input = []
+    considered_cata = copy.copy(reader.fieldnames)
+    considered_cata.remove('Id')
+    for row in reader:
+        train_input_row = []
+        for cata in reader.fieldnames:
+            if cata not in SPEC_CATA:
+                if is_number(row[cata]):
+                    train_input_row.append(float(row[cata]))
+                else:
+                    train_input_row.append(0.0)
+            elif cata == 'Product_Info_2':
+                train_input_row.append(float(int(row[cata], 16)))
+            elif cata == 'Response':
+                train_input_row.append(float(row[cata]))
+        train_input.append(train_input_row)
+    cor = numpy.corrcoef(train_input, rowvar=0)
+    considered_cata = sorted(considered_cata, key=lambda x: -abs(cor[considered_cata.index(x)][-1]))
+    considered_cata.remove('Response')
+    while len(considered_cata) > NUM_CONSIDERED_CATA:
+        considered_cata.pop()
+    print(considered_cata)
+
+clf = MLPClassifier(verbose=True)
+with open('input/train.csv', newline='') as train_file:
+    reader = csv.DictReader(train_file)
     train_input = []
     train_output = []
 
     for row in reader:
         train_input_row = []
-        for cata in CATA:
-            train_input_row.append(float(row[cata]))
+        for cata in considered_cata:
+            if cata not in SPEC_CATA:
+                if is_number(row[cata]):
+                    train_input_row.append(float(row[cata]))
+                else:
+                    train_input_row.append(0.0)
+            elif cata == 'Product_Info_2':
+                train_input_row.append(float(int(row[cata], 16)))
         train_input.append(train_input_row)
         train_output.append(int(row['Response']))
 
     clf.fit(train_input, train_output)
 
 with\
-    open('input/test.csv', newline='') as input_file,\
-    open('output.csv', 'w') as output_file:
+    open('input/train.csv', newline='') as input_file,\
+    open('output2.csv', 'w') as output_file:
 
     reader = csv.DictReader(input_file)
     predict_input = []
@@ -38,12 +74,23 @@ with\
 
     for row in reader:
         predict_input_row = []
-        for cata in CATA:
-            predict_input_row.append(float(row[cata]))
+        for cata in considered_cata:
+            if cata not in SPEC_CATA:
+                if is_number(row[cata]):
+                    predict_input_row.append(float(row[cata]))
+                else:
+                    predict_input_row.append(0.0)
+            elif cata == 'Product_Info_2':
+                predict_input_row.append(float(int(row[cata], 16)))
         predict_input.append(predict_input_row)
         predict_id.append(row['Id'])
 
     predict_output = clf.predict(predict_input)
+    counter = 0
+    for i in range(len(train_output)):
+        if train_output[i] == predict_output[i]:
+            counter += 1
+    print(str(counter), '/', str(len(train_output)))
 
     output_file.write('"Id","Response"')
     for i in range(len(predict_id)):
