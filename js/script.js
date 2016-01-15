f090d8d5c5e236e7bbb65dae962dc261aa22f8a0
Ril Jian <rilislearning@gmail.com>
2016-01-15 18:36:33 +0800

    infoWindow, reportForm

--- js/script.js
+++ js/script.js
@@ -6,8 +6,16 @@ var currentMarker;
 var currentPosition;
 var autoMove = true;
 var accidentMarkers = [];
+var accidentInfo = [];
 var firebase = new Firebase("https://glaring-torch-4222.firebaseio.com/accident/");
-var situation_marker = [
+var infoWindow;
+var accidentType = [
+    '事故',
+    '施工',
+    '管制',
+    '障礙'
+];
+var situation_marker_icon = [
     'img/red-dot.png',
     'img/blue-dot.png',
     'img/purple-dot.png',
@@ -16,7 +24,10 @@ var situation_marker = [
 
 function applyPosition(position) {
     'use strict';
-    var latlng = {lat: position.coords.latitude, lng: position.coords.longitude};
+    var latlng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
     currentMarker.setPosition(latlng);
     currentPosition = latlng;
     if (autoMove) {
@@ -27,12 +38,15 @@ function applyPosition(position) {
 
 function positionErrorHandler(positionError) {
     'use strict';
-    window.alert('Error: ' + positionError.message);
+    window.console.log('Error: (' + positionError.code + ')' + positionError.message);
 }
 
 function mapsAPILoaded() {
     'use strict';
-    var defaultPosition = {lat: 25.039820, lng: 121.512001};
+    var defaultPosition = {
+        lat: 25.039820,
+        lng: 121.512001
+    };
     map = new google.maps.Map(document.getElementById('map'), {
         center: defaultPosition,
         zoom: 16
@@ -47,14 +61,29 @@ function mapsAPILoaded() {
         zIndex: google.maps.Marker.MAX_ZINDEX + 1,
         icon: 'img/1452554285_car.png'
     });
+    infoWindow = new google.maps.InfoWindow({
+        maxWidth: 200
+    });
     firebase.on('child_added', function (snapshot) {
-        var data = snapshot.val();
-        accidentMarkers.push(new google.maps.Marker({
-            position: {lat: data.latitude, lng: data.longitude},
-            map: map,
-            animation: google.maps.Animation.DROP,
-            icon: situation_marker[data.type]
-        }));
+        var data = snapshot.val(),
+            marker = new google.maps.Marker({
+                position: {
+                    lat: data.latitude,
+                    lng: data.longitude
+                },
+                map: map,
+                animation: google.maps.Animation.DROP,
+                icon: situation_marker_icon[data.type]
+            });
+        marker.addListener('click', function () {
+            infoWindow.setContent(
+                '<h4>' + accidentType[accidentInfo[accidentMarkers.indexOf(this)].type] + '</h4>' +
+                    '<p>' + accidentInfo[accidentMarkers.indexOf(this)].description + '</p>'
+            );
+            infoWindow.open(map, this);
+        });
+        accidentInfo.push(data);
+        accidentMarkers.push(marker);
     });
 }
 
@@ -64,19 +93,22 @@ function relocate() {
     $('#position-btn').fadeOut();
 }
 
-function accidentReport(btn) {
+function accidentReport(type, desc) {
     'use strict';
+    /*
     var lat = currentPosition.lat + 0.000001 * Math.ceil(Math.random() * 8948),
         lng = currentPosition.lng + 0.000001 * Math.ceil(Math.random() * 13239);
+    */
     $.get('https://roads.googleapis.com/v1/snapToRoads', {
         key: 'AIzaSyCquZV94BIFX8D4J75AG8ZWGICNOJOGb60',
-        path: lat + ',' + lng
+        path: currentPosition.lat + ',' + currentPosition.lng
     }, function (data) {
         firebase.push({
             timestamp: Math.ceil(Date.now() / 1000),
             latitude: data.snappedPoints[0].location.latitude,
             longitude: data.snappedPoints[0].location.longitude,
-            type: Number($(btn).val())
+            description: desc,
+            type: type
         });
     });
 }
@@ -97,6 +129,15 @@ $(document).ready(function () {
         }
     });
     $('.situation-btn').click(function () {
-        accidentReport(this);
+        $('#report-form').toggleClass('scale');
+        $('#report-confirm').val($(this).val());
+    });
+    $('#report-cancel').click(function () {
+        $('#report-form').toggleClass('scale');
+    });
+    $('#report-confirm').click(function () {
+        accidentReport(Number($(this).val()), $('#desc').val());
+        $('#report-form').toggleClass('scale');
+        $('#situation-btn-0').click();
     });
 });
